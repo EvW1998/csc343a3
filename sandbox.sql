b46cfb7735b8810f7beeca6dca7fac4aedaa0794
Zhijian Wang <zhijian.wang.98@gmail.com>
2017-12-05 23:27:13 -0500

    sandbox

---
+++ sandbox.sql
@@ -0,0 +1,31 @@
+DROP SCHEMA IF EXISTS quizschema CASCADE;
+CREATE SCHEMA quizschema;
+SET SEARCH_PATH TO quizschema;
+
+CREATE OR REPLACE FUNCTION is_not_overlap(upper_bound integer, lower_bound integer, q_id integer)
+	RETURNS void AS
+$func$
+DECLARE
+	rec RECORD
+
+BEGIN
+	FOR rec IN SELECT lower_range FROM numeric_question_hints WHERE id = q_id
+	LOOP
+		RAISE NOTICE '%', rec.lower_range;
+	END LOOP
+END
+$func$ LANGUAGE plpgsql STABLE STRICT;
+
+CREATE TABLE numeric_question_hints(
+	-- ID of the question that the answer related to
+    id INT NOT NULL,
+	-- hint for this answer
+    hint VARCHAR(1000),
+	-- The lower range for this hint
+    lower_range INT NOT NULL,
+	-- The upper range
+    upper_range INT NOT NULL,
+	
+	-- Make sure no two hints have same range in one question
+    UNIQUE(id, lower_range, upper_range)
+);
